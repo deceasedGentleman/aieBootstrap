73d949b5d43917f46853d95e33b23d7fec9dfe57
Sean McCarthy <maxrewind99@gmail.com>
2019-04-18 15:02:30 +1000

    manually edited in improvements from quats

--- graphics3D/graphics3DApp.cpp
+++ graphics3D/graphics3DApp.cpp
@@ -1,84 +1,99 @@
+#define GLM_ENABLE_EXPERIMENTAL
+
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
+#include <glm/gtx/transform.hpp>
+#include <glm/gtx/quaternion.hpp>
+
 #include "graphics3DApp.h"
 #include "Gizmos.h"
 #include "Input.h"
-#include <glm/glm.hpp>
-#include <glm/ext.hpp>
 #include "FlyCamera.h"
 
-using glm::vec3;
-using glm::vec4;
-using glm::mat4;
+
+using namespace glm;
 using aie::Gizmos;
 
-graphics3DApp::graphics3DApp() {
-  
-}
-
-graphics3DApp::~graphics3DApp() {
+graphics3DApp::graphics3DApp()
+{
 
 }
 
-bool graphics3DApp::startup() {
-	
-   m_camera = new FlyCamera(glm::pi<float>() * 0.25f,
-                           16 / 9,
+graphics3DApp::~graphics3DApp()
+{
+
+}
+
+bool graphics3DApp::startup()
+{
+
+   m_camera = new FlyCamera(radians(45.0f), 16 / 9,
                            vec3(10), vec3(0), vec3(0, 1, 0));
    setShowCursor(false);
 
-	setBackgroundColour(0.25f, 0.25f, 0.25f);
+   setBackgroundColour(0.25f, 0.25f, 0.25f);
 
-	// initialise gizmo primitive counts
-	Gizmos::create(10000, 10000, 10000, 10000);
-	return true;
+   // initialise gizmo primitive counts
+   Gizmos::create(10000, 10000, 10000, 10000);
+   return true;
 }
 
-void graphics3DApp::shutdown() {
+void graphics3DApp::shutdown()
+{
 
-	Gizmos::destroy();
+   Gizmos::destroy();
 }
 
-void graphics3DApp::update(float deltaTime) {
+void graphics3DApp::update(float deltaTime)
+{
 
-	// wipe the gizmos clean for this frame
-	Gizmos::clear();
+   // wipe the gizmos clean for this frame
+   Gizmos::clear();
 
-	// draw a simple grid with gizmos
-	vec4 white(1);
-	vec4 black(0, 0, 0, 1);
-	for (int i = 0; i < 21; ++i) {
-		Gizmos::addLine(vec3(-10 + i, 0, 10),
-						vec3(-10 + i, 0, -10),
-						i == 10 ? white : black);
-		Gizmos::addLine(vec3(10, 0, -10 + i),
-						vec3(-10, 0, -10 + i),
-						i == 10 ? white : black);
-	}
-   glm::vec2 windowCentre = { getWindowWidth()/2, getWindowHeight()/2 };
-   Gizmos::add2DAABB(windowCentre, { 4,4 }, { 1,1,1,1 });
-	// add a transform so that we can see the axis
-	Gizmos::addTransform(mat4(1));
+   // draw a simple grid with gizmos
+   vec4 white(1);
+   vec4 black(0, 0, 0, 1);
+   for (int i = 0; i < 21; ++i)
+   {
+      Gizmos::addLine(vec3(-10 + i, 0, 10),
+                  vec3(-10 + i, 0, -10),
+                  i == 10 ? white : black);
+      Gizmos::addLine(vec3(10, 0, -10 + i),
+                  vec3(-10, 0, -10 + i),
+                  i == 10 ? white : black);
+   }
+   glm::vec2 mid = { getWindowWidth() / 2, getWindowHeight() / 2 };
+   float l = 4.0f;
+   Gizmos::add2DAABB(mid, vec2(l), vec4(1));
+   Gizmos::add2DLine(vec2(mid.x + 4, mid.y), vec2(mid.x - 4, mid.y), vec4(1));
 
-	// quit if we press escape
-	aie::Input* input = aie::Input::getInstance();
+   // add a transform so that we can see the axis
+   Gizmos::addTransform(mat4(1));
 
-	if (input->isKeyDown(aie::INPUT_KEY_ESCAPE))
-		quit();
+
+   // quit if we press escape
+   aie::Input* input = aie::Input::getInstance();
+
+   if (input->isKeyDown(aie::INPUT_KEY_ESCAPE))
+      quit();
 
    m_camera->update(deltaTime);
 }
 
-void graphics3DApp::draw() {
+void graphics3DApp::draw()
+{
 
-	// wipe the screen to the background colour
-	clearScreen();
+   // wipe the screen to the background colour
+   clearScreen();
 
    // update perspective in case window resized
    m_camera->setPerspective(glm::pi<float>() * 0.25f,
-                            (float)getWindowWidth() / (float)getWindowHeight(),
+      (float)getWindowWidth() / (float)getWindowHeight(),
                             0.1f, 1000.f);
 
    //draw 3D gizmos using the camera class
    m_camera->draw();
-   // draw 2D gizmos using an orthogonal projection matrix (or screen dimensions)
+
+   // draw 2D gizmos using screen dimensions
    Gizmos::draw2D((float)getWindowWidth(), (float)getWindowHeight());
 }
